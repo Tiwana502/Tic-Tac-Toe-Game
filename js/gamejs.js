2536d08fef3cf036201469b37a8e5850e2eba2f4
Tiwana Jones <Advance4life@Yahoo.com>
2019-10-15 11:46:13 -0400

    added id's and flushed out event listener

--- js/gamejs.js
+++ js/gamejs.js
@@ -1,4 +1,5 @@
 console.log("wired up");
+const moves = [];
 
 let playerOneTurn = true;
 
@@ -9,6 +10,7 @@ for (let i = 0; i < gridDivArray.length; i++) {
 
     const gameBtn = document.createElement("button");
     gameBtn.innerText = "";
+    gameBtn.id = i
 
     gridDivArray[i].appendChild(gameBtn);
 
@@ -18,24 +20,29 @@ for (let i = 0; i < gridDivArray.length; i++) {
     gameBtn.addEventListener("click", function() {
 
         if (playerOneTurn === true) {
-            if (this.innerText !== 'O' && this.innerText !== 'X') {
-                this.innerText = "X";
-                playerOneTurn = false;
-            }
-
-
-
-
-
-        }
-
-
-        if (this.innerText !== 'X' && this.innerText !== 'O') {
+            console.log(this)
+            moves[this.id] = 'X'
+                // if (this.innerText !== 'O' && this.innerText !== 'X') {
+            this.innerText = "X";
+            playerOneTurn = false;
+            // }        }
+        } else {
+            moves[this.id] = 'O'
             this.innerText = "O";
 
             playerOneTurn = true;
         }
 
+        // if (this.innerText !== 'X' && this.innerText !== 'O') {
+
+        // }
+        console.log(moves)
     });
 
 };
+
+if (moves[0] === moves[1] && moves[1] === moves[2]) {
+
+} else if
+
+else if
