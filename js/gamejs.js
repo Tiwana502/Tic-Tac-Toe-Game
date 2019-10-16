e3269c3efdb65d4505f9f28ca1730762979ff972
Tiwana Jones <Advance4life@Yahoo.com>
2019-10-16 09:48:34 -0400

    Game 98% complte

--- js/gamejs.js
+++ js/gamejs.js
@@ -2,6 +2,7 @@ console.log("wired up");
 const moves = [];
 
 let playerOneTurn = true;
+const winnerAlert = document.querySelector("#winnerAlert");
 
 // grab the divs using query selector  so that we can work with them in the DOM
 const gridDivArray = document.querySelectorAll('.grid');
@@ -25,12 +26,14 @@ for (let i = 0; i < gridDivArray.length; i++) {
                 // if (this.innerText !== 'O' && this.innerText !== 'X') {
             this.innerText = "X";
             playerOneTurn = false;
-            // }        }
+            checkForWin()
+                // }        }
         } else {
             moves[this.id] = 'O'
             this.innerText = "O";
 
             playerOneTurn = true;
+            checkForWin()
         }
 
         // if (this.innerText !== 'X' && this.innerText !== 'O') {
@@ -41,8 +44,67 @@ for (let i = 0; i < gridDivArray.length; i++) {
 
 };
 
-if (moves[0] === moves[1] && moves[1] === moves[2]) {
+const checkForWin = () => {
+    if (moves[0] !== undefined &&
+        moves[0] === moves[1] &&
+        moves[1] === moves[2]) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[0]} Wins!`)
+    } else if ((moves[3] !== undefined &&
+            moves[3] === moves[4] &&
+            moves[4] === moves[5])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[3]} Wins!`)
+    } else if ((moves[6] !== undefined &&
+            moves[6] === moves[7] &&
+            moves[7] === moves[8])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[4]} Wins!`)
 
-} else if
+    } else if ((moves[0] !== undefined &&
+            moves[0] === moves[3] &&
+            moves[3] === moves[6])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[5]} Wins!`)
 
-else if
+    } else if ((moves[1] !== undefined &&
+            moves[1] === moves[4] &&
+            moves[4] === moves[7])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[6]} Wins!`)
+
+    } else if ((moves[2] !== undefined &&
+            moves[2] === moves[5] &&
+            moves[5] === moves[8])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[7]} Wins!`)
+
+    } else if ((moves[0] !== undefined &&
+            moves[0] === moves[4] &&
+            moves[4] === moves[8])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[8]} Wins!`)
+
+    } else if ((moves[2] !== undefined &&
+            moves[2] === moves[4] &&
+            moves[4] === moves[6])) {
+        winnerAlert.innerText = "You Win!";
+        console.log(`${ moves[9]} Wins!`)
+
+
+
+
+
+
+
+
+    }
+
+
+
+    // } else if (moves[0] !== moves[1] && moves[0] !== moves[2]) {
+
+    //     alert("Sorry,You did not Win!");
+
+    // }
+}
