261820866812efc9cde517970a378df6a503b724
Tiwana Jones <Advance4life@Yahoo.com>
2019-10-16 14:32:09 -0400

    Established winning player and Tie game results

--- js/gamejs.js
+++ js/gamejs.js
@@ -4,6 +4,8 @@ const moves = [];
 let playerOneTurn = true;
 const winnerAlert = document.querySelector("#winnerAlert");
 
+
+
 // grab the divs using query selector  so that we can work with them in the DOM
 const gridDivArray = document.querySelectorAll('.grid');
 
@@ -48,57 +50,55 @@ const checkForWin = () => {
     if (moves[0] !== undefined &&
         moves[0] === moves[1] &&
         moves[1] === moves[2]) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[0]} Wins!`)
+        winnerAlert.innerText = `${ moves[2]} Wins!`;
+
     } else if ((moves[3] !== undefined &&
             moves[3] === moves[4] &&
             moves[4] === moves[5])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[3]} Wins!`)
+        winnerAlert.innerText = `${ moves[5]} Wins!`;
+
     } else if ((moves[6] !== undefined &&
             moves[6] === moves[7] &&
             moves[7] === moves[8])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[4]} Wins!`)
+        winnerAlert.innerText = `${ moves[8]} Wins!`;
+
 
     } else if ((moves[0] !== undefined &&
             moves[0] === moves[3] &&
             moves[3] === moves[6])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[5]} Wins!`)
+        winnerAlert.innerText = `${ moves[6]} Wins!`;
+
 
     } else if ((moves[1] !== undefined &&
             moves[1] === moves[4] &&
             moves[4] === moves[7])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[6]} Wins!`)
+        winnerAlert.innerText = `${ moves[7]} Wins!`;
+
 
     } else if ((moves[2] !== undefined &&
             moves[2] === moves[5] &&
             moves[5] === moves[8])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[7]} Wins!`)
+        winnerAlert.innerText = `${ moves[8]} Wins!`;
+
 
     } else if ((moves[0] !== undefined &&
             moves[0] === moves[4] &&
             moves[4] === moves[8])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[8]} Wins!`)
+        winnerAlert.innerText = `${ moves[8]} Wins!`;
+
 
     } else if ((moves[2] !== undefined &&
             moves[2] === moves[4] &&
             moves[4] === moves[6])) {
-        winnerAlert.innerText = "You Win!";
-        console.log(`${ moves[9]} Wins!`)
+        winnerAlert.innerText = `${ moves[6]} Wins!`;
 
-
-
-
-
-
-
-
+    } else if (moves.length === 9) {
+        winnerAlert.innerText = "Tie Game";
     }
+
+
+
+
 
 
 
