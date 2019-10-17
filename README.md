6c92a10528609c0b859fff9e48c68d47efa959ff
Tiwana Jones <Advance4life@Yahoo.com>
2019-10-17 14:25:15 -0400

    Readme draft

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Tic-Tac-Toe-Game
-The user of this Tic-Tac-Toe rendition will enjoy the ability to play as one of two cultural icons.
-The user that plays as Malcolm X knows that X marks the spot in order to win.
-The user that plays as Oprah challeges Malcolm's hypothesis with an "Oh, Really?!" for the the win.
-Oprah takes on the Beyonce' philosophy and believes that women run the world as a user "Oh what a feeling world domination brings!"
-Malcolm X users believe in the Xpanse/expanse of patriarchy.
+Hi, I'm a moderately young, creative, and up & coming full stack Web Developer in the making. My name is Tiwana Jones. I decided to use the Iconic Malcolm X and Oprah Winfrey as the player one and player two in my rendition of the Tic Tac Toe game because of the obvious X and O in their name. I always wanted to know how a conversation between the two would go. Although I respect and admire both greatly, I never agreed with either fully. I have a dual nature as a Gemini in the zodiac , therefore, let the games begin.
+
+User stories
+- The user of this Tic-Tac-Toe game rendition will enjoy the ability to play as one of two cultural icons; either Malcolm X or Oprah Winfrey.
+- The user that plays as Malcolm X knows that X marks the spot in order to win.
+- The user that plays as Oprah will counter or challege  Malcolm's hypothesis with an "Oh, Really?!", for the the win.
+-  The Oprah player adheres to the Beyonce' philosophy , believing that women run the world ,and as that user, "Oh what a feeling world domination brings!"
+-  The Malcolm X users believe in the Xpanse/expanse of patriarchy, planting their X flag every where the user may strategicly choose to go.
+
+The technologies I implemented to bring my version of the game to fruition were a Wireframe, css file, Html file and Javascript file.
+
+![Third eye vision](./images/Wireframe.png)
+
+
--- js/gamejs.js
+++ js/gamejs.js
@@ -4,107 +4,98 @@ const moves = [];
 let playerOneTurn = true;
 const winnerAlert = document.querySelector("#winnerAlert");
 
-
-
 // grab the divs using query selector  so that we can work with them in the DOM
-const gridDivArray = document.querySelectorAll('.grid');
+const gridDivArray = document.querySelectorAll(".grid");
 
 for (let i = 0; i < gridDivArray.length; i++) {
-
     const gameBtn = document.createElement("button");
     gameBtn.innerText = "";
-    gameBtn.id = i
+    gameBtn.id = i;
 
     gridDivArray[i].appendChild(gameBtn);
 
-
-
-
     gameBtn.addEventListener("click", function() {
-
         if (playerOneTurn === true) {
-            console.log(this)
-            moves[this.id] = 'X'
-                // if (this.innerText !== 'O' && this.innerText !== 'X') {
+            console.log(this);
+            moves[this.id] = "X";
+            // if (this.innerText !== 'O' && this.innerText !== 'X') {
             this.innerText = "X";
             playerOneTurn = false;
-            checkForWin()
-                // }        }
+            checkForWin();
+            // }        }
         } else {
-            moves[this.id] = 'O'
+            moves[this.id] = "O";
             this.innerText = "O";
 
             playerOneTurn = true;
-            checkForWin()
+            checkForWin();
         }
 
         // if (this.innerText !== 'X' && this.innerText !== 'O') {
 
         // }
-        console.log(moves)
+        console.log(moves);
     });
-
-};
+}
 
 const checkForWin = () => {
-    if (moves[0] !== undefined &&
+    if (
+        moves[0] !== undefined &&
         moves[0] === moves[1] &&
-        moves[1] === moves[2]) {
-        winnerAlert.innerText = `${ moves[2]} Wins!`;
-
-    } else if ((moves[3] !== undefined &&
-            moves[3] === moves[4] &&
-            moves[4] === moves[5])) {
-        winnerAlert.innerText = `${ moves[5]} Wins!`;
-
-    } else if ((moves[6] !== undefined &&
-            moves[6] === moves[7] &&
-            moves[7] === moves[8])) {
-        winnerAlert.innerText = `${ moves[8]} Wins!`;
-
-
-    } else if ((moves[0] !== undefined &&
-            moves[0] === moves[3] &&
-            moves[3] === moves[6])) {
-        winnerAlert.innerText = `${ moves[6]} Wins!`;
-
-
-    } else if ((moves[1] !== undefined &&
-            moves[1] === moves[4] &&
-            moves[4] === moves[7])) {
-        winnerAlert.innerText = `${ moves[7]} Wins!`;
-
-
-    } else if ((moves[2] !== undefined &&
-            moves[2] === moves[5] &&
-            moves[5] === moves[8])) {
-        winnerAlert.innerText = `${ moves[8]} Wins!`;
-
-
-    } else if ((moves[0] !== undefined &&
-            moves[0] === moves[4] &&
-            moves[4] === moves[8])) {
-        winnerAlert.innerText = `${ moves[8]} Wins!`;
-
-
-    } else if ((moves[2] !== undefined &&
-            moves[2] === moves[4] &&
-            moves[4] === moves[6])) {
-        winnerAlert.innerText = `${ moves[6]} Wins!`;
-
+        moves[1] === moves[2]
+    ) {
+        winnerAlert.innerText = `${moves[2]} Wins!`;
+    } else if (
+        moves[3] !== undefined &&
+        moves[3] === moves[4] &&
+        moves[4] === moves[5]
+    ) {
+        winnerAlert.innerText = `${moves[5]} Wins!`;
+    } else if (
+        moves[6] !== undefined &&
+        moves[6] === moves[7] &&
+        moves[7] === moves[8]
+    ) {
+        winnerAlert.innerText = `${moves[8]} Wins!`;
+    } else if (
+        moves[0] !== undefined &&
+        moves[0] === moves[3] &&
+        moves[3] === moves[6]
+    ) {
+        winnerAlert.innerText = `${moves[6]} Wins!`;
+    } else if (
+        moves[1] !== undefined &&
+        moves[1] === moves[4] &&
+        moves[4] === moves[7]
+    ) {
+        winnerAlert.innerText = `${moves[7]} Wins!`;
+    } else if (
+        moves[2] !== undefined &&
+        moves[2] === moves[5] &&
+        moves[5] === moves[8]
+    ) {
+        winnerAlert.innerText = `${moves[8]} Wins!`;
+    } else if (
+        moves[0] !== undefined &&
+        moves[0] === moves[4] &&
+        moves[4] === moves[8]
+    ) {
+        winnerAlert.innerText = `${moves[8]} Wins!`;
+    } else if (
+        moves[2] !== undefined &&
+        moves[2] === moves[4] &&
+        moves[4] === moves[6]
+    ) {
+        winnerAlert.innerText = `${moves[6]} Wins!`;
     } else if (moves.length === 9) {
         winnerAlert.innerText = "Tie Game";
     }
 
-
     const reset = document.getElementById("reset");
-    reset.addEventListener('click', function() {
+    reset.addEventListener("click", function() {
         location.reload();
-    })
+    });
 };
-
-
-
 
 // } else if (moves[0] !== moves[1] && moves[0] !== moves[2]) {
 
